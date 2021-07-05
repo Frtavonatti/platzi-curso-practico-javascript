f9cc9162a3a0750bbf4492bbdc01f8ac194189f2
Felipe Rivera <frtavonatti@gmail.com>
2021-07-04 21:37:58 -0400

    Reto: Funciones más inteligentes completado

--- descuentos.js
+++ descuentos.js
@@ -6,11 +6,28 @@ function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById('InputPrice');
     const price = inputPrice.value;
 
-    const inputDiscount = document.getElementById('InputDiscount');
-    const discount = inputDiscount.value;
+    // const inputDiscount = document.getElementById('InputDiscount');
+    // const discount = inputDiscount.value;
+
+    const inputCupon = document.getElementById('InputCoupon');
+    const couponValue = inputCupon.value;
+
+    const coupons = [
+        'cupon1',
+        'cupon2',
+        'cupon3'
+    ]
+    
+    if (couponValue === coupons[0]) {
+        discount = 10
+    } else if (couponValue === coupons[1]) {
+        discount = 15
+    } else if (couponValue === coupons[2]){
+        discount = 20 
+    } else console.log('El cupon ingresado no existe')
 
     const precioConDescuento = calcularPrecioConDescuento(price, discount);
-    
+
     const result = document.getElementById('ResultP');
     result.innerText = `El precio con descuento son: $${precioConDescuento}`
 }
