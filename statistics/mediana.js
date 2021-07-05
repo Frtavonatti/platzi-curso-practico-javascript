9787413d8c6945ffcf059c27e17a3d131d614c83
Felipe Rivera <frtavonatti@gmail.com>
2021-07-05 08:58:10 -0400

    Reto función calcular mediana terminado

--- statistics/mediana.js
+++ statistics/mediana.js
@@ -1,10 +1,17 @@
 const lista = [10, 20, 30, 40, 45, 5000]
+const lista2 = [2, 123, 34, 131, 32, 0.4, 89]
 
-function calcularMediana(arr) { 
-    let mitadLista = arr.length / 2;
-    if (arr.length % 2 === 0) {
-        return (Math.floor(mitadLista) + Math.round(mitadLista)) / 2
-    } return arr[Math.floor(mitadLista)]
+function ordenarLista(arr) {
+    return arr.sort((a, b) => a - b)
 }
 
-console.log(calcularMediana(lista))
+function calcularMediana(arr) { 
+    ordenarLista(arr);
+    let mitadLista = parseInt(arr.length / 2);
+    if (arr.length % 2 === 0) {
+        return (arr[mitadLista] + arr[mitadLista - 1]) / 2
+    } return arr[mitadLista]
+}
+
+console.log(calcularMediana(lista2))
+// console.log(ordenarLista(lista2))
