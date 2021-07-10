09206084c56ad5fd00ed66f2705cf1a3e49d2a98
Felipe Rivera <frtavonatti@gmail.com>
2021-07-10 15:10:16 -0400

    Desafio moda terminado

--- statistics/mediana.js
+++ statistics/mediana.js
@@ -1,17 +1,23 @@
 const lista = [10, 20, 30, 40, 45, 5000]
-const lista2 = [2, 123, 34, 131, 32, 0.4, 89]
+const lista2 = [2, 123, 34, 131, 32, 0.4, 89, 1]
 
 function ordenarLista(arr) {
     return arr.sort((a, b) => a - b)
 }
 
+function esPar(num) {
+    if (num % 2 === 0) {
+        return true
+    } return false
+}
+
 function calcularMediana(arr) { 
     ordenarLista(arr);
     let mitadLista = parseInt(arr.length / 2);
-    if (arr.length % 2 === 0) {
+    if (esPar(arr.length)) {
         return (arr[mitadLista] + arr[mitadLista - 1]) / 2
     } return arr[mitadLista]
 }
 
+console.log(ordenarLista(lista2))
 console.log(calcularMediana(lista2))
-// console.log(ordenarLista(lista2))
