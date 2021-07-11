de24e49d9ac6981b32b16be3d994ddfaa86e3d02
Felipe Rivera <frtavonatti@gmail.com>
2021-07-11 13:04:42 -0400

    Agregue la funcionalidad para calcular la mediana del 10% de los salarios, pero termina afectado al array general (corta 2 de sus elementos)

--- salarios/analisis.js
+++ salarios/analisis.js
@@ -6,14 +6,13 @@ const sortArray = (arr) => arr.sort((x, y) => x - y)
 const salariosColOrdenados = sortArray(salariosColombia)
 console.log(salariosColOrdenados)
 
-const esPar = (num) => (num % 2 === 0)
-
 function calcularMediana (arr) {
+    const esPar = (num) => (num % 2 === 0)
     const half = parseInt(arr.length / 2)
-    if (esPar(arr)) {
+    if (esPar(arr.length)) {
         const half2 = half - 1
-        const mediumOfHalfs = (half + half2) / 2 
-        return arr[mediumOfHalfs]
+        const mediumOfHalfs = (arr[half ]+ arr[half2]) / 2 
+        return mediumOfHalfs
     } else {
         const halfElement = arr[half]
         return halfElement
@@ -22,3 +21,14 @@ function calcularMediana (arr) {
 
 const medianaSalariosColombia = calcularMediana(salariosColOrdenados)
 console.log(medianaSalariosColombia)
+
+const topTenPercent = (arr) => {
+    const tenPercent = parseInt(arr.length * 0.1)    
+    const startIndex = arr.length - tenPercent
+    return arr.splice(startIndex, tenPercent)
+} 
+
+const topTenPercentColombiaSalaries = topTenPercent(salariosColOrdenados)
+console.log(topTenPercentColombiaSalaries)
+
+console.log(calcularMediana(topTenPercentColombiaSalaries))
